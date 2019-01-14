2e07782b909972221be9cef0a13452c57c7f416d
unknown <brettj85@gmail.com>
2019-01-13 21:18:15 -0600

    up to spider

--- textBasedGame/Program.cs
+++ textBasedGame/Program.cs
@@ -6,7 +6,71 @@ namespace textBasedGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Welcome to the cavern of secrets");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            System.Threading.Thread.Sleep(3000);
+            
+            for (int i = 0; i <= 3; i++)
+            {
+            Console.Beep();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("You enter a dark cavern out of curiosity. It is dark and you can only make out a small stick on the floor.");
+            Console.WriteLine();
+            Console.Write("Do you take it y/n ");
+            string stickAnswer = Console.ReadLine();
+            
+            Console.Clear();
+            System.Threading.Thread.Sleep(2000);
+            Console.Beep();
+
+            int stick = 0;
+            if (stickAnswer == "y" || stickAnswer == "Y" || stickAnswer == "yes" || stickAnswer == "Yes"){
+                stick = 1;
+                Console.WriteLine("You have taken the stick!");
+            }
+            else
+            {
+                stick = 0;
+                Console.WriteLine("You did not take the stick");
+            }
+
+            Console.Clear();
+            System.Threading.Thread.Sleep(2000);
+            Console.Beep();
+
+            Console.WriteLine("As you proceed further into the cave, you see a small glowing object");
+            Console.WriteLine();
+            Console.Write("Do you approach the object? y/n ");
+            string approachAnswer = Console.ReadLine();
+            
+            Console.Clear();
+            System.Threading.Thread.Sleep(2000);
+            Console.Beep();
+
+            if (approachAnswer == "y" || approachAnswer == "Y" || approachAnswer == "yes" || approachAnswer == "Yes"){
+                Console.WriteLine("You approach the object...");
+                Console.Clear();
+                System.Threading.Thread.Sleep(1000);
+                Console.Beep();
+                Console.WriteLine("As you draw closer, you begin to make out the object as an eye!");
+                Console.Clear();
+                System.Threading.Thread.Sleep(1000);
+                Console.Beep();
+                Console.WriteLine("The eye belongs to a giant spider!");
+                Console.WriteLine();
+                Console.Write("Do you try to fight it? y/n ");    
+            }
+
+
+
+
+            Console.ReadLine();
         }
+
     }
 }
