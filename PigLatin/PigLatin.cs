76f43bc6e2ea73e8a9fa54df128bab786c42eec2
unknown <brettj85@gmail.com>
2019-01-14 21:01:09 -0600

    up to spec 6

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,6 +7,10 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
+            Console.WriteLine("enter a word to translate ");
+            string userInput = Console.ReadLine();
+            string newWord = makeLowerCase(userInput);
+            Console.WriteLine(newWord);
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
@@ -15,7 +19,50 @@ namespace PigLatin
         public static string TranslateWord(string word)
         {
             // your code goes here
-            return word;
+            string firstLetter = word.Substring(0,1);
+            string restWord = word.Substring(1);
+            string translatedWord = (restWord + firstLetter + "ay");
+            return translatedWord;
+        }
+
+        public static string TranslateWordVowel(string word)
+        {
+            // your code goes here
+            int firstVowelIndex = word.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u', 'y'});
+            string firstPart = word.Substring(0, firstVowelIndex);
+            string secondPart = word.Substring(firstVowelIndex);
+            string translatedWord = (secondPart + firstPart + "ay");
+
+            return translatedWord;
+        }
+
+        public static string startsWithVowel(string word)
+        {
+            // your code goes here
+            int firstVowelIndex = word.IndexOfAny(new char[] {'a', 'e', 'i', 'o', 'u', 'y'});
+            string last = word.Substring(0,1);
+            string first = word.Substring(1);
+            if (firstVowelIndex == 0)
+            {
+
+                string translatedWord = (last + first +"ay");
+
+                return translatedWord;   
+            }
+            else
+            {
+                string notAVowel = (first + last + "ay");
+                return notAVowel;
+            }
+            
+        }
+        public static string makeLowerCase(string word)
+        {
+            string convertLower = word.ToLower();
+            string firstLetter = convertLower.Substring(0,1);
+            string restWord = convertLower.Substring(1);
+            string translatedWord = (restWord + firstLetter + "ay");
+            return translatedWord;            
         }
     }
 }
