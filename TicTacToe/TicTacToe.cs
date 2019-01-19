4d645f08d21c2f36fc6f674fa7f846a490f010bd
unknown <brettj85@gmail.com>
2019-01-19 00:53:55 -0600

    finished tic tac toe, going to write it recursive and hopefully test all the win conditions

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -146,12 +146,12 @@ namespace TicTacToe
         }
         public static void printBoard(string[,] board)
         {
-            Console.WriteLine("  " + "0 " + " 1 " + " 2");
-            Console.WriteLine("0  " + board[0, 0] + "| " + board[0, 1] + " |" + board[0, 2]);
-            Console.WriteLine(" " + "--------");
-            Console.WriteLine("1  " + board[1, 0] + "| " + board[1, 1] + " |" + board[1, 2]);
-            Console.WriteLine(" " + "--------");
-            Console.WriteLine("2  " + board[2, 0] + "| " + board[2, 1] + " |" + board[2, 2]);
+            Console.WriteLine("  " + "0" + " 1 " + "2");
+            Console.WriteLine("0 " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
+            Console.WriteLine(" " + "-----");
+            Console.WriteLine("1 " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
+            Console.WriteLine(" " + "-----");
+            Console.WriteLine("2 " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2]);
         }
     }
 }
---
+++ keepScoreTicTT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace keepScoreTicTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
