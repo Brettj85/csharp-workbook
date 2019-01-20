9e923321fca9b10f38a3e62a27d3ea4a4f79cbb4
unknown <brettj85@gmail.com>
2019-01-19 21:33:38 -0600

    finished recursive play, score keeping, a basic AI. still to go... give the AI some skills, make ties happen if the remaiining moves result in a tie

--- keepScoreTicTT/Program.cs
+++ keepScoreTicTT/Program.cs
@@ -6,76 +6,137 @@ namespace TicTacToe
     {
         public static void Main()
         {
-            playGame();
+            playGame(0, 0, 0);
 
         }
 
-        public static void playGame()
+        public static void playGame(int x, int o, int t)
         {
-            bool win = false; //If the win is true, game is over
+            //keep track of wins and ties
+            int xWins = x;
+            int oWins = o;
+            int timesTied = t;
+
+
+            bool win = false;
+            string playerTurn = "X";
+            string[,] board = new string[3, 3];
+            bool automaticPlay = true;
+            bool playAgain = true;
+            //keeps track of the question of manual turns
+            bool marker = true;
+            //keeps track of a full turn rotation
+            int turnTicker = 1;
+            int turn = 1;
             while (!win)
             {
-                string playerTurn = "X";
-                int turn = 0;
-                string[,] board = new string[3, 3];
+                //Make the Board
+                printBoard(board);
 
-                for (int i = 1; i <= 9; i++)
+                //determine if its an NPC or human, and collect the row and column choice. 
+                int row;
+                int column;
+                if (automaticPlay)
                 {
-                    printBoard(board); //Make the Board
-                    Console.WriteLine("Player " + playerTurn);
-                    Console.WriteLine("Enter Row:"); //player input
-                    int row = Int32.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter Column:");//player input
-                    int column = Int32.Parse(Console.ReadLine());
-                    Console.Clear();
-                    //check if that spot is taken
-                    if (isTaken(board, row, column))
-                    {
-                        Console.WriteLine("Sorry that spot is taken");
-                    }
-                    else//otherwise place a marker and change the turn
-                    {
-                        board[row, column] = playerTurn;
-                        playerTurn = (playerTurn == "X") ? "O" : "X";
-                    }
-
-                    if ((2 % i) == 0) //increment turn every two moves
-                    {
-                        turn++;
-                    }
-
-                    string winner = "n";
-                    if (i >= 3)
-                    {
-                        Console.WriteLine("checking winner");
-                        winner = checkWin(board);
-                    }
-
-                    if (i >= 7)
-                    {
-                        winner = checkTie(board);
-                    }
-
-                    if (winner == "X" || winner == "O")
-                    {
-                        win = true;
-                        break;
-                    }
+                    row = autoSelect();
+                    column = autoSelect();
+                    string computerGuess = (playerTurn + " guess is: " + row.ToString() + " , " + column.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine(computerGuess);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    row = manualSelect(playerTurn, marker);
+                    marker = false;
+                    column = manualSelect(playerTurn, marker);
+                    marker = true;
                 }
 
-                return;
+                //check if that spot is taken
+                if (isTaken(board, row, column))
+                {
+                    Console.WriteLine("Sorry that spot is taken");
+                }
+                else
+                {
+                    board[row, column] = playerTurn;
+                    playerTurn = (playerTurn == "X") ? "O" : "X";
+                    turnTicker++;
+                }
+
+                //increment "turn" every two moves
+                if ((turnTicker % 2) == 0)
+                {
+                    turn++;
+                }
+
+                //check for win
+                string winner = "n";
+                string tie = "n";
+                if (turn >= 3)
+                {
+                    tie = checkTie(board);
+                    winner = checkWin(board);
+                }
+
+                if (winner == "X")
+                {
+                    xWins++;
+                    Console.Clear();
+                    printBoard(board);
+                    Console.WriteLine("X is the winner!");
+                    Console.WriteLine();
+                    Console.WriteLine("There have been {0} Ties the score is", timesTied);
+                    Console.WriteLine("X: {0}", xWins);
+                    Console.WriteLine("O: {0}", oWins);
+                    System.Threading.Thread.Sleep(10000);
+                    win = true;
+                }
+                else if (winner == "O")
+                {
+                    oWins++;
+                    Console.Clear();
+                    printBoard(board);
+                    Console.WriteLine("O is the winner!");
+                    Console.WriteLine();
+                    Console.WriteLine("There have been {0} Ties the score is", timesTied);
+                    Console.WriteLine("X: {0}", xWins);
+                    Console.WriteLine("O: {0}", oWins);
+                    System.Threading.Thread.Sleep(10000);
+                    win = true;
+                }
+                else if (tie == "T")
+                {
+                    timesTied++;
+                    Console.Clear();
+                    printBoard(board);
+                    Console.WriteLine("We have a Tie!");
+                    Console.WriteLine("There have been {0} Ties the score is", timesTied);
+                    Console.WriteLine("X: {0}", xWins);
+                    Console.WriteLine("O: {0}", oWins);
+                    System.Threading.Thread.Sleep(10000);
+                    win = true;
+                }
+
+
+            }
+            if (playAgain)
+            {
+                playGame(xWins, oWins, timesTied);
             }
         }
+
 
         public static bool isTaken(string[,] board, int row, int column)//checks to see if a given tic tac toe spot is occupied
         {
-            if (String.IsNullOrEmpty(board[row, column]))
+            if (!String.IsNullOrEmpty(board[row, column]) || !String.IsNullOrWhiteSpace(board[row, column]))
             {
-                return false;
+                return true;
             }
             else
             {
-                return true;
+                return false;
             }
         }
 
@@ -84,6 +145,7 @@ namespace TicTacToe
             string checkRows = checkHorizontal(board);
             string checkColumns = checkVertical(board);
             string checkCorner2Corner = checkDiagonal(board);
+
             if (checkRows == "X" || checkColumns == "X" || checkCorner2Corner == "X")
             {
                 return "X";
@@ -102,14 +164,20 @@ namespace TicTacToe
             string isWinner = "n";
             for (int i = 0; i <= 2; i++)
             {
-                if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2] && !String.IsNullOrEmpty(board[i, 0]))
+                if (!String.IsNullOrWhiteSpace(board[i, 0]) && !String.IsNullOrEmpty(board[i, 0]))
                 {
-                    isWinner = board[i, 0];
+                    if (board[i, 0] == board[i, 1] && board[i, 0] == board[i, 2])
+                    {
+                        isWinner = board[i, 0];
+                    }
+                }
+                else
+                {
+                    isWinner = "n";
                 }
             }
             Console.WriteLine(isWinner);
             return isWinner;
-
         }
 
         public static string checkVertical(string[,] board)
@@ -117,23 +185,22 @@ namespace TicTacToe
             string isWinner = "n";
             for (int i = 0; i <= 2; i++)
             {
-                if (board[0, i] == board[1, i] && board[0, i] == board[2, i] && !String.IsNullOrEmpty(board[0, i]))
+                if (board[0, i] == board[1, i] && board[0, i] == board[2, i] && !String.IsNullOrEmpty(board[0, i]) && !String.IsNullOrWhiteSpace(board[0, 0]))
                 {
                     isWinner = board[0, i];
                 }
             }
             return isWinner;
-
         }
 
         public static string checkDiagonal(string[,] board)
         {
             string isWinner = "n";
-            if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2] && !String.IsNullOrEmpty(board[0, 0]))
+            if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2] && !String.IsNullOrEmpty(board[0, 0]) && !String.IsNullOrWhiteSpace(board[0, 0]))
             {
                 isWinner = board[0, 0];
             }
-            else if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0] && !String.IsNullOrEmpty(board[0, 2]))
+            else if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0] && !String.IsNullOrEmpty(board[0, 2]) && !String.IsNullOrWhiteSpace(board[0, 2]))
             {
                 isWinner = board[0, 2];
             }
@@ -142,8 +209,23 @@ namespace TicTacToe
 
         public static string checkTie(string[,] board)
         {
-
-            return "n";
+            string isTie = "n";
+            for (int i = 0; i <= 2; i++)
+            {
+                if (String.IsNullOrWhiteSpace(board[i, 0]) || String.IsNullOrWhiteSpace(board[i, 1]) || String.IsNullOrWhiteSpace(board[i, 2]))
+                {
+                    if (String.IsNullOrEmpty(board[i, 0]) || String.IsNullOrEmpty(board[i, 1]) || String.IsNullOrEmpty(board[i, 2]))
+                    {
+                        isTie = "n";
+                        break;
+                    }
+                }
+                else
+                {
+                    isTie = "T";
+                }
+            }
+            return isTie;
         }
 
         public static void printBoard(string[,] board)
@@ -175,6 +257,41 @@ namespace TicTacToe
                 return checkValue;
             }
         }
-
+        private static readonly Random getrandom = new Random();
+        public static int autoSelect()
+        {
+            int aiGuess = getrandom.Next(0, 31);
+            int guess = 0;
+            if (aiGuess <= 10)
+            {
+                guess = 0;
+            }
+            else if (aiGuess <= 20)
+            {
+                guess = 1;
+            }
+            else
+            {
+                guess = 2;
+            }
+            return guess;
+        }
+        public static int manualSelect(string player, bool marker)
+        {
+            int selection;
+            if (marker)
+            {
+                Console.WriteLine("player {0}'s turn!", player);
+                Console.Write("Enter Row: ");
+                selection = Int32.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.Write("Enter Column: ");
+                selection = Int32.Parse(Console.ReadLine());
+            }
+            return selection;
+        }
     }
 }
+
