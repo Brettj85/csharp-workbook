424e0a75876d227df44be9b5eb646a4db688bc6e
unknown <brettj85@gmail.com>
2019-01-26 13:49:38 -0600

    finished shell

---
+++ sort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrToSort = new int[20] { 3, 64, 5, 6, 677, 8, 889, 44, 33, 2, 2221, 44, 55, 6, 77, 88, 89, 45, 32, 6 };
+            int[] sortedArray = SortArray(arrToSort);
+
+        }
+
+        public static int[] SortArray(int[] array)
+        {
+            //sort the array here
+            return array;
+        }
+    }
+}
