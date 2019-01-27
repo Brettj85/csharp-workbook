26d5d1be1e4f43f710ac84eb3710dbcdab206d33
unknown <brettj85@gmail.com>
2019-01-27 15:41:01 -0600

    made an unfortunate assumption about how the sort will happenso things are divided into chunks of 9 and 11... 10 and 10 fixes it but it coded all the way through look at the end before coding in the future

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -7,14 +7,38 @@ using System.Linq;
 public class SortWithMultiThread
 {
 
-    public static void PartialSort(int[] arr)
+    public static int[] PartialSort(int[] arr)
     {
-        for (int i = 0; i < 10; i++)
-        {
+        int[] arrayToSort = arr;
 
-            // Yield the rest of the time slice.
-            Thread.Sleep(0);
+        int[] smallerThanAverage = new int[arrayToSort.Length];
+        int average;
+        int indexOfLowerHalf = 0;
+        int indexOfHigherHalf = arrayToSort.Length - 1;
+        int sum = arrayToSort.Sum();
+        average = sum / arrayToSort.Length;
+
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            if (arrayToSort[i] <= average)
+            {
+                smallerThanAverage[indexOfLowerHalf] = arrayToSort[i];
+                indexOfLowerHalf++;
+            }
+            else
+            {
+                smallerThanAverage[indexOfHigherHalf] = arrayToSort[i];
+                indexOfHigherHalf--;
+            }
         }
+        int[] asyncSortThis = new int[(smallerThanAverage.Length / 2) - 1];
+        int j = 0;
+        for (int i = (smallerThanAverage.Length / 2) + 1; j < asyncSortThis.Length; i++)
+        {
+            asyncSortThis[j] = smallerThanAverage[i];
+            j++;
+        }
+        return arr;
     }
 
     public static void Main()
@@ -51,13 +75,14 @@ public class SortWithMultiThread
         }
 
 
-        // The constructor for the Thread.
+        // The constructor for a Thread.        
         Thread sortingThread = new Thread(new ThreadStart(() => PartialSort(asyncSortThis)));
         //start thread
         sortingThread.Start();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i <= (smallerThanAverage.Length / 2) + 1; i++)
         {
+
             // Yield the rest of the time slice.
             Thread.Sleep(0);
         }
