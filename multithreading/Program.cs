86e2b73533615ceeba9c1e9196e1c7e7adbe8a86
unknown <brettj85@gmail.com>
2019-01-27 00:39:16 -0600

    finished splitting the array into a larger # array and smaller (partial sort) about to go async on both halves

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading;
+using System.Linq;
+
 
 
 public class SortWithMultiThread
 {
-    public static void PartialSort()
+
+    public static void PartialSort(int[] arr)
     {
         for (int i = 0; i < 10; i++)
         {
@@ -16,16 +19,44 @@ public class SortWithMultiThread
 
     public static void Main()
     {
+        //split-half sort array into 2 sub arrays 1 containg the numbers that are > than average and one <= average 
+        int[] arrayToSort = new int[20] { 13, 57, 34, 151, 70, 68, 222, 149, 64, 165, 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
+
+        int[] smallerThanAverage = new int[20];
+        int average;
+        int indexOfHalf = 0;
+        int sum = arrayToSort.Sum();
+        average = sum / arrayToSort.Length;
+
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            if (arrayToSort[i] <= sum)
+            {
+                smallerThanAverage[indexOfHalf] = arrayToSort[i];
+                indexOfHalf++;
+            }
+            else
+            {
+                smallerThanAverage[20 - indexOfHalf] = arrayToSort[i];
+            }
+        }
+        int[] asyncSortThis = new int[10];
+        int j = 0;
+        for (int i = 11; i < smallerThanAverage.Length; i++)
+        {
+            asyncSortThis[i] = smallerThanAverage[j];
+            j++;
+        }
+
 
         // The constructor for the Thread.
-        Thread sortingThread = new Thread(new ThreadStart(PartialSort));
-
+        Thread sortingThread = new Thread(new ThreadStart(() => PartialSort(asyncSortThis)));
         //start thread
         sortingThread.Start();
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 10; i++)
         {
-
+            // Yield the rest of the time slice.
             Thread.Sleep(0);
         }
 
