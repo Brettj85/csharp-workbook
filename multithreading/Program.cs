10c3fed1e5013546d0e18c4cdabef689a2a20fb8
unknown <brettj85@gmail.com>
2019-01-26 21:00:34 -0600

    shell of multithread done may need 1 more thread

---
+++ multithreading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+
+public class SortWithMultiThread
+{
+    public static void PartialSort()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+
+            // Yield the rest of the time slice.
+            Thread.Sleep(0);
+        }
+    }
+
+    public static void Main()
+    {
+
+        // The constructor for the Thread.
+        Thread sortingThread = new Thread(new ThreadStart(PartialSort));
+
+        //start thread
+        sortingThread.Start();
+
+        for (int i = 0; i < 4; i++)
+        {
+
+            Thread.Sleep(0);
+        }
+
+        //join threads
+        sortingThread.Join();
+        //Join has returned
+        Console.ReadLine();
+    }
+}
