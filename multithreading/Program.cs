026bac857496f5e9623a2d7f5a3b44f526f6799a
unknown <brettj85@gmail.com>
2019-01-27 19:14:29 -0600

    there is a more modern way of doing this

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 public class SortWithMultiThread
 {
-
+    //used for sorting an arr of unique values
     public static int[] PartialSort(int[] arr)
     {
         int[] arrayToSort = arr;
@@ -38,39 +38,21 @@ public class SortWithMultiThread
             asyncSortThis[j] = smallerThanAverage[i];
             j++;
         }
-        return arr;
+        int[] smallerArr = asyncSortThis;
+        return smallerArr;
     }
 
     public static void Main()
     {
         //split-half sort array into 2 sub arrays 1 containg the numbers that are > than average and one <= average 
-        int[] arrayToSort = new int[20] { 13, 57, 34, 151, 70, 68, 222, 149, 64, 165, 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
+        int[] inputArray = new int[20] { 13, 57, 34, 151, 70, 68, 222, 149, 64, 165, 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
+        int[] splitHalf = PartialSort(inputArray);
 
-        int[] smallerThanAverage = new int[arrayToSort.Length];
-        int average;
-        int indexOfLowerHalf = 0;
-        int indexOfHigherHalf = arrayToSort.Length - 1;
-        int sum = arrayToSort.Sum();
-        average = sum / arrayToSort.Length;
-
-        for (int i = 0; i < arrayToSort.Length; i++)
-        {
-            if (arrayToSort[i] <= average)
-            {
-                smallerThanAverage[indexOfLowerHalf] = arrayToSort[i];
-                indexOfLowerHalf++;
-            }
-            else
-            {
-                smallerThanAverage[indexOfHigherHalf] = arrayToSort[i];
-                indexOfHigherHalf--;
-            }
-        }
-        int[] asyncSortThis = new int[(smallerThanAverage.Length / 2) - 1];
+        int[] asyncSortThis = new int[(splitHalf.Length / 2) - 1];
         int j = 0;
-        for (int i = (smallerThanAverage.Length / 2) + 1; j < asyncSortThis.Length; i++)
+        for (int i = (splitHalf.Length / 2) + 1; j < asyncSortThis.Length; i++)
         {
-            asyncSortThis[j] = smallerThanAverage[i];
+            asyncSortThis[j] = splitHalf[i];
             j++;
         }
 
@@ -80,12 +62,10 @@ public class SortWithMultiThread
         //start thread
         sortingThread.Start();
 
-        for (int i = 0; i <= (smallerThanAverage.Length / 2) + 1; i++)
-        {
+        PartialSort(int[] arr);
+        // Yield the rest of the time slice.
+        Thread.Sleep(0);
 
-            // Yield the rest of the time slice.
-            Thread.Sleep(0);
-        }
 
         //join threads
         sortingThread.Join();
