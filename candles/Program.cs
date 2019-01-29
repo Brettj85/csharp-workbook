4286038a694fcb36a94cdae53db6cf6c1e902f23
unknown <brettj85@gmail.com>
2019-01-28 19:43:36 -0600

    candles 1 loop done

---
+++ candles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberOfCandles = { 1, 2, 4, 8, 8, 6 };
+            int tallest = 1;
+            int howManyCandles = numberOfCandles.Length;
+            int j = 1;
+            foreach (var num in numberOfCandles)
+            {
+                if (num > tallest)
+                {
+                    tallest = num;
+                    j = 1;
+                }
+                else if (num == tallest)
+                {
+                    j++;
+                }
+            }
+
+            Console.WriteLine("she can blow out {0} candles!", j);
+        }
+    }
+}
