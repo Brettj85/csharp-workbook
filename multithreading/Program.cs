dfa5d5d724251d188db3c38320a7d705e11f6171
unknown <brettj85@gmail.com>
2019-01-29 18:33:10 -0600

    have the basic gameplan

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -7,7 +7,20 @@ public class Program
 {
     public static void Main()
     {
+        var results = new List<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 };
+        double average = results.Average();
 
+        //split it into 2 with higher than average and lower values being in one or the other respectivly
+
+        //keep splitting till each group has 2 (account for an odd amount of numbers)
+        // note on this all groups must remain in order 
+
+        //sort the groups of 2 at the same time
+
+        //combine all the groups together
+
+
+        //output result
     }
 }
 
