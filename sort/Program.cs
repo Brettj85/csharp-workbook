25296bab76dcd9929e61a7f4418ab8c0519d2bd4
unknown <brettj85@gmail.com>
2019-01-30 17:00:52 -0600

    dont know what i was doing there restarting it... yayy another one down to the wire

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.ComponentModel;
+
 
 namespace sort
 {
@@ -8,32 +8,11 @@ namespace sort
         //restarting
         static void Main(string[] args)
         {
-            int[] arrayToSort = new int[20] { 3, 64, 5, 6, 677, 8, 889, 44, 33, 2, 2221, 44, 55, 6, 77, 88, 89, 45, 32, 6 };
-            SortArray(arrayToSort);
-            for (int i = 0; i < arrayToSort.Length; i++)
-            {
-                Console.Write("{0}, ", arrayToSort[i]);
-            }
 
         }
 
         public static void Sortparts(int[] array)
         {
-            //sort the array here
-            int min;
-            for (int i = 0; i < array.Length; i++)
-            {
-                min = i;
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[j] > array[min])
-                    {
-                        min = j;
-                        int placeHolder = array[i];
-                        array[i] = array[min];
-                    }
-                }
-            }
         }
     }
 }
