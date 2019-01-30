ae86364a5cddc62e0e2196c09ab6c3bae42e122d
unknown <brettj85@gmail.com>
2019-01-29 22:08:33 -0600

    basic structure

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -7,20 +7,52 @@ public class Program
 {
     public static void Main()
     {
-        var results = new List<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 };
-        double average = results.Average();
+        var value_to_sort = new List<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 };
+        var split_lists = SplitList(value_to_sort);
+        var returned_results = SplitMore(split_lists).Result;
+        // dont need this - var sorted_results = SortSplitValues(split_lists).Result;
+        string final_list = MergeSortedResults(returned_results);
 
-        //split it into 2 with higher than average and lower values being in one or the other respectivly
+        Console.WriteLine(final_list);
+    }
+    private static object SplitList(List<int> unsorted)
+    {
+        double average = unsorted.Average();
+        var partial_sort = new ReturnedList();
+        foreach (var num in unsorted)
+        {
+            if (num <= average)
+            {
+                partial_sort.less_than.Add(num);
+            }
+            else
+            {
+                partial_sort.greater_than.Add(num);
+            }
+        }
+        return partial_sort;
+    }
+    public static List<int> MergeSortedResults(List<object> nums)
+    {
 
-        //keep splitting till each group has 2 (account for an odd amount of numbers)
-        // note on this all groups must remain in order 
+        return num;
+    }
+    public async Task<List<object>> SplitMore(object nums)
+    {
+        //take in # of things to split
+        //loop through and do the following until there are 2 lists with 1 item each 
+        //do this by splitting into x # of tasks with x # of tasks each. need a good peice of reusable code here. need to think about it...
+        //split the larger side and the smaller side at the same time 
+        //return 4 "returned lists" then 6 then so-on
+        var something = new List<object> { nums, nums };
+        await Task.WhenAll();
+        return something;
+    }
 
-        //sort the groups of 2 at the same time
-
-        //combine all the groups together
-
-
-        //output result
+    private class ReturnedList
+    {
+        public List<int> less_than = new List<int>();
+        public List<int> greater_than = new List<int>();
     }
 }
 
