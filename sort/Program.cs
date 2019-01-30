b7b7e7b4794adcfde38af22186b2ebfe944f2b9e
unknown <brettj85@gmail.com>
2019-01-30 17:09:14 -0600

    structure doneish

--- sort/Program.cs
+++ sort/Program.cs
@@ -8,11 +8,14 @@ namespace sort
         //restarting
         static void Main(string[] args)
         {
+            list<int> origin_list = new list<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 }
+            StartSsplitting(origin_list);
 
         }
 
-        public static void Sortparts(int[] array)
+        public static string StartSsplitting(list<int> origin_list)
         {
+
         }
     }
 }
