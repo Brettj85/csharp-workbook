984ebe6a0708ff699dbc6daf6ccf1381ee654e0e
unknown <brettj85@gmail.com>
2019-01-30 16:56:39 -0600

    have 30 min left then need to write a simple sort for home work

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -7,46 +7,85 @@ namespace AsyncSort
 {
     public class Program
     {
-        public static void Main()
-        {
-            var value_to_sort = new List<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 };
-            var split_lists = ReturnSortedValues(value_to_sort).results;
-            var final_list = MergeSortedResults(split_lists);
-
-            Console.WriteLine(final_list);
-        }
-
-        public static async Task<ReturnedObjects> SplitList(List<int> nums)
-        {
-            var convert_too_class = new ReturnedList();
-            var list_of_returns = new ReturnedObjects();
-
-            await ();
-        }
-        public async task<ReturnedObjects> ReturnSortedValues(List<int> nums)
-        {
-            double average = nums.Average();
-            List<ReturnedList> results = new List<ReturnedList>();
-            List<ReturnedObjects> return_objects = new List<ReturnedObjects>();
-            foreach (var num in nums)
-            {
-                if (num > average)
-                {
-                    results.less_than.add(num);
-                }
-            }
-        }
-
-        public class ReturnedList
+        public class Lists
         {
             public List<int> less_than = new List<int>();
             public List<int> greater_than = new List<int>();
         }
-
-        public class ReturnedObjects
+        public class Objects
         {
-            public List<ReturnedList> less_than = new List<ReturnedList>();
-            public List<ReturnedList> greater_than = new List<ReturnedList>();
+            public List<Lists> lists_objects = new List<Lists>();
+        }
+        public class Tasks
+        {
+            public List<Task<Lists>> task_list = new List<Task<Lists>>();
+        }
+        public static void Main()
+        {
+            var value_to_sort = new List<int> { 4, 5, 10, 12, 14, 20, 1, 205, 26, 31, 120, 2, 3, 72, 40, 100, 140 };
+            Program start = new Program();
+            string sorted_list = start.ReturnSortedValues(value_to_sort);
+            Console.WriteLine(sorted_list);
+        }
+        private string ReturnSortedValues(List<int> numbers)
+        {
+            var sort_me = new Objects();
+            var semi_sorted = new Objects();
+            Lists starting_lists = new Lists();
+            int i = 0;
+            int j = 0;
+            foreach (var num in numbers)
+            {
+                if (num <= numbers.Average())
+                {
+                    starting_lists.less_than[i] = num;
+                    i++;
+                }
+                else
+                {
+                    starting_lists.greater_than[j] = num;
+                    i++;
+                }
+            }
+            sort_me.lists_objects[0] = starting_lists;
+
+            while (sort_me.lists_objects.Count != numbers.Count / 2)
+            {
+                var return_tasks = Sorted(sort_me).Result;
+                foreach (var tsk in return_tasks.task_list)
+                {
+                    semi_sorted.lists_objects.Add(tsk.Result);
+                }
+
+                // foreach (var num in nums)
+                // {
+                //     taskListGreater.Add(GreaterThanAverage(num, avg));
+                // }
+
+                // await Task.WhenAll(return_tasks);
+                // var sort =
+                // sort_me = sort;
+            }
+
+            return "print this";
+        }
+
+        public static async Task<List<Tasks>> Sorted(Objects double_me)
+        {
+            var semi_sorted = new Objects();
+            semi_sorted = double_me;
+            var task_list = new List<Task<Objects>>();
+            for (int i = 0; i > semi_sorted.lists_objects.Count; i++)
+            {
+                task_list.Add(AsyncListSort(semi_sorted.lists_object[i]));
+            }
+            await Task.WhenAll(task_list);
+
+            return task_list;
+        }
+        public static async Task<List<Tasks>> AsyncListSort(List<Objects> double_me)
+        {
+
         }
     }
 }
