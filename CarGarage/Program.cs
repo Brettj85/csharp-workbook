0de9e7708fb3fe83b31323c213db60488ccc916c
unknown <brettj85@gmail.com>
2019-01-31 00:34:18 -0600

    finished car garage

---
+++ CarGarage/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person("Bill");
+        Car blueCar = new Car("blue", person, 1);
+
+        blueCar.AddPerson(person, 2);
+        Garage smallGarage = new Garage(2);
+
+        smallGarage.ParkCar(blueCar, 0);
+        Console.WriteLine(smallGarage.Cars);
+    }
+    class Person
+    {
+        public Person(string initial_name)
+        {
+            first_name = initial_name;
+        }
+
+        public string first_name { get; private set; }
+    }
+    class Car
+    {
+        private Person[] person;
+        int spot;
+
+        public Car(string initialColor, Person initialName, int initialSpot)
+        {
+            this.person = new Person[4];
+            this.Color = initialColor;
+            this.person[0] = initialName;
+            this.spot = initialSpot;
+        }
+        public string Color { get; private set; }
+        public void AddPerson(Person initialName, int where)
+        {
+            person[where] = initialName;
+        }
+    }
+
+
+    class Garage
+    {
+        private Car[] cars;
+
+        public Garage(int initialSize)
+        {
+            Size = initialSize;
+            this.cars = new Car[initialSize];
+        }
+
+        public int Size { get; private set; }
+
+        public void ParkCar(Car car, int spot)
+        {
+            cars[spot] = car;
+        }
+
+        public string Cars
+        {
+            get
+            {
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    if (cars[i] != null)
+                    {
+                        Console.WriteLine(String.Format("The {0} car is in spot {1}. There are  ", cars[i].Color, i, Person[i].person));
+                    }
+                }
+                return "That's all!";
+            }
+        }
+    }
+}
---
+++ CarGarage2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarGarage2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Person
+    {
+        private List<string> names;
+
+        private Person(string next_person)
+        {
+            string first_name = next_person;
+            this.names.Add(first_name);
+        }
+        public string next_person { get; private set; }
+    }
+    class Car
+    {
+        //variables
+        private List<int> capacity;
+        public Car(/*properties */)
+        {
+            //Do Work....
+        }
+        //get and set properties
+        public string next_person { get; private set; }
+    }
+    class Garage
+    {
+
+    }
+}
