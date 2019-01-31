06b0f81ac2f14da286a74607d61432829faa3afb
unknown <brettj85@gmail.com>
2019-01-30 23:35:40 -0600

    finished cp1 factorial

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace FizzBuzz
+namespace checkpoint1Factorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter a number for factorial");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = userNumber - 1; i > 0; i--)
+            {
+                userNumber = userNumber * i;
+            }
+            Console.WriteLine("the factorial is {0}", userNumber);
+
         }
     }
 }
