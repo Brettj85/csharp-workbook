268aa6a27da3ee1c029900f7d5e46023ba620640
unknown <brettj85@gmail.com>
2019-01-31 00:23:36 -0600

    basic idea mapped

---
+++ guessingGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //find a random # between 1/10
+
+            //take in 4 guesses and match against the number
+            Console.WriteLine("Hello World!");
+        }
+    }
+    /*Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
+            If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program 
+            is behaving correctly, you can display the secret number on the console first.) */
+}
