bdda32b00cb851a557b37cda3f7daa18166417a3
unknown <brettj85@gmail.com>
2019-01-30 23:47:43 -0600

    candles

---
+++ candles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberOfCandles = { 1, 3, 2, 6, 7, 9, 9, 8, 7, 3, 9, };
+            int largest = 0;
+            int howManyCandles = 1;
+            foreach (var num in numberOfCandles)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                    howManyCandles = 1;
+                }
+                else if (num == largest)
+                {
+                    howManyCandles++;
+                }
+            }
+            Console.WriteLine("they can blow out {0} candles", howManyCandles);
+        }
+    }
+}
