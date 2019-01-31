aa1c1bc6d9a94ec8418f6c7e1181d90f818117c7
unknown <brettj85@gmail.com>
2019-01-30 23:38:23 -0600

    ok now find biggest is done

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 
-namespace checkpoint1Factorial
+namespace FizzBuzz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a number for factorial");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
-            for (int i = userNumber - 1; i > 0; i--)
-            {
-                userNumber = userNumber * i;
-            }
-            Console.WriteLine("the factorial is {0}", userNumber);
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
