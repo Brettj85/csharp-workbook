e2f3269ace70d0e25b082e2d920aae7e1d980b22
unknown <brettj85@gmail.com>
2019-02-01 00:43:56 -0600

    got the number and cheating done

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -6,12 +6,33 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            //find a random # between 1/10
-
+            RandomNumber guess_me = new RandomNumber();
+            guess_me.Cheat(3);
             //take in 4 guesses and match against the number
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("{0}", guess_me.guess_this);
         }
     }
+    class RandomNumber
+    {
+        private Random rnd = new Random();
+
+        public RandomNumber()
+        {
+            guess_this = rnd.Next(1, 10);
+        }
+        public void Cheat(int selected_number)
+        {
+            this.guess_this = selected_number;
+        }
+        public int guess_this { get; private set; }
+    }
+
+    class PlayGame
+    {
+
+
+    }
+
     /*Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
             If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program 
             is behaving correctly, you can display the secret number on the console first.) */
