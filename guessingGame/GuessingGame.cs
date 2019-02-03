d41faf6c11f4c7ee64781e05534deb9f79f817d7
unknown <brettj85@gmail.com>
2019-02-03 14:24:35 -0600

    structure done starting play controller

---
+++ guessingGame/GuessingGame.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace guessingGame
+{
+    class RandomNumber
+    {
+        //get a random number and store the properties of the random  
+        public RandomNumber(CheatController cheat)
+        {
+            CheatController check = cheat;
+
+            //if  = 1337
+
+        }
+
+
+        /*Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
+                If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program 
+                is behaving correctly, you can display the secret number on the console first.) */
+    }
+    class PlayGame
+    {
+
+    }
+}
