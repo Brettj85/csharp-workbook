96c65278ce422500597f5df6e803ed99602bfc95
unknown <brettj85@gmail.com>
2019-02-03 01:47:24 -0600

    ruff outline on new version of guessing game up

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace guessingGame
 {
@@ -6,143 +7,70 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            RandomNumber guess_me = new RandomNumber();
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
-            PlayerStatistics player = new PlayerStatistics(name, age);
-            Console.WriteLine("cheat code?");
-            guess_me.Cheat(Console.ReadLine());
-            PlayGame start = new PlayGame(player, guess_me);
-            start.guess_loop(start.number, player);
-
-            //take in 4 guesses and match against the number
-            Console.WriteLine("{0}", guess_me.guess_this);
+            //run game controller           
         }
     }
-    class RandomNumber
-    {
-        private bool cheat = false;
-        private Random rnd = new Random();
-        private int guess_it;
-        public RandomNumber()
-        {
-            guess_this = rnd.Next(1, 10);
-            guess_it = guess_this;
-        }
-        public void Cheat(string selected_number)
-        {
-            try
-            {
-                this.guess_it = Convert.ToInt32(selected_number);
-                this.cheat = true;
-            }
-            catch (System.FormatException)
-            {
-                Console.WriteLine("incorrect CheatCode");
-                Console.WriteLine("A random # has been generated");
-                Console.ReadLine();
-            }
 
-        }
-        public int RequestNumber()
-        {
-            return guess_it;
-        }
-        public int guess_this { get; private set; }
-    }
-
+    // keep track of player data games played cheating lives in each game
     class PlayerStatistics
     {
+        private string player_first;
+        private string player_last;
+        private int[] guessing_game = new int[] { 0, 0, 0, 0 };
         private int lives = 4;
-        private bool cheat = false;
-
-        public PlayerStatistics(string player_name, int player_age)
+        public PlayerStatistics(string first, string last)
         {
-            this.name = player_name;
-            this.age = player_age;
+
+            player_first = first;
+            player_last = last;
         }
 
-        public string name { get; private set; }
-        public int age { get; private set; }
-
-        public int CheckLives()
+        //statistics generator
+        public void GenerateStatistics()
         {
-            return this.lives;
-        }
-
-        public void Cheat(int selected_number)
-        {
-            try
+            List<string> game_list = new List<string>() { "*                Guessing Game                *" };
+            //List<int[]> stats_list = new List<int[]>(){};
+            // for times sake will have to adapt this to add games to a list and loop through
+            //if i have time i will make it generate spacing based off word length
+            Console.Clear();
+            string top_bot = "***********************************************";
+            Console.WriteLine("* Name: {1}, {0} *", player_first, player_last);
+            for (int i = 0; i == game_list.Count; i++)
             {
-                this.lives = Convert.ToInt32(selected_number);
-                cheat = true;
+                Console.WriteLine(top_bot);
+                Console.WriteLine("*                                             *");//45
+                Console.WriteLine(game_list[i]);
+                Console.WriteLine("*Played: {0}                                *", "00" + guessing_game[i]);//make sure all numbers come out to XXX ie 001, 002, 201, 999 max
+                Console.WriteLine("*Won: {0}                        Lost: {1}*", "00" + guessing_game[i + 1], "00" + guessing_game[i + 2]);
+                Console.WriteLine("*Cheated: {0}                               *", "00" + guessing_game[i + 3]);
             }
-            catch (System.FormatException)
-            {
-                Console.WriteLine("incorrect CheatCode");
-                Console.WriteLine("Good Try, press enter to exit.");
-                Console.ReadLine();
-            }
-
+            Console.WriteLine(top_bot);
         }
-
     }
 
-    class PlayGame
+    //Game controller
+    class ProgramController
     {
-        private bool cheat = false;
-        private int lives;
-        private bool winner;
-        public PlayGame(PlayerStatistics player, RandomNumber guess_me)
-        {
-            PlayerStatistics player_data = player;
-            number = guess_me.RequestNumber();
-            lives = player.CheckLives();
-            int num_to_guess = number;
-        }
+        //run software until quit
+        private bool quit = false;
 
-        public void guess_loop(int num_to_guess, PlayerStatistics player_data)
+        public ProgramController()
         {
-            bool number_guessed = false;
-            //bool winner = false;
-            int guess_this = number;
-            while (lives < 0 && !number_guessed)
+            //run software until quit
+            while (quit)
             {
-                Console.WriteLine("Guess the number");
-                int player_guess = Convert.ToInt32(Console.ReadLine());
-                try
-                {
-                    if (player_guess == guess_this)
-                    {
-                        Console.WriteLine("you have Won");
-                        number_guessed = true;
-                        winner = true;
-                    }
-                    else if (lives > 0)
-                    {
-                        Console.WriteLine("Try Again! you have {0} lives left", lives);
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have {0} lives left, Game Over", lives);
-                        Console.WriteLine("Cheat?");
-                        player_data.Cheat(Convert.ToInt32(Console.ReadLine()));
-                    }
-                }
-                catch (System.Exception)
-                {
-                    Console.WriteLine("Good Game");
-                    Console.ReadLine();
-                }
+
             }
         }
 
-        public object player_data { get; private set; }
-        public int number { get; private set; }
-        /*Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
-                If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program 
-                is behaving correctly, you can display the secret number on the console first.) */
+        //number guess game
     }
+
+    //Cheat controller
+    class CheatController
+    {
+        private bool is_cheating = false;
+
+    }
+    //number guess game
 }
