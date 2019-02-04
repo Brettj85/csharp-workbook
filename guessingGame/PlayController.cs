db4933a27675415d50231269ad7bb3a86a7a3cf8
unknown <brettj85@gmail.com>
2019-02-04 00:03:58 -0600

    got it working 100%

--- guessingGame/PlayController.cs
+++ guessingGame/PlayController.cs
@@ -7,32 +7,108 @@ namespace guessingGame
     class ProgramController
     {
         private bool quit = false;
+        PlayerStatistics data_file;
+        CheatController cheats;
+
 
         public ProgramController(PlayerStatistics user_data)
         {
-            PlayerStatistics data_file = user_data;
-            CheatController cheats = new CheatController();
+            this.data_file = user_data;
+            this.cheats = new CheatController();
+        }
+        public void Start()
+        {
 
             //run software until quit
-
-            while (quit)
+            while (!quit)
             {
+                Console.Clear();
                 // build menu
-                //take in a choice
-                //play game of choosing
-                //capture the data back contained in userdata
-                // pass same userdata into as many games as needed and 
+                Console.WriteLine("***********************************************");
+                Console.WriteLine("*      Play Guessing Game : press g           *");
+                Console.WriteLine("*              Cheat: press c                 *");
+                Console.WriteLine("*              Exit: press x                  *");
+                Console.WriteLine("*          Current Stats: press s             *");
+                Console.WriteLine("***********************************************");
+                string choice = "void";
+                while (choice == "void")
+                {
+                    try
+                    {
+                        choice = Console.ReadLine();
+
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("Invalid Selection. Please try again.");
+                    }
+                }
+
+                if (choice == "g")
+                {
+                    int[] results;
+                    PlayGuessingGame play = new PlayGuessingGame(data_file, cheats);
+                    results = play.PlayGame();
+                    int i = 0;
+                    foreach (var item in results)
+                    {
+                        data_file.guessing_game[i] = data_file.guessing_game[i] + item;
+                        i++;
+                    }
+                }
+                else if (choice == "c")
+                {
+                    Console.WriteLine("Enter Cheat Code");
+                    try
+                    {
+                        cheats.AddCheat(Console.ReadLine());
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("Invalid Entry!");
+                    }
+                }
+                else if (choice == "s")
+                {
+                    data_file.GenerateStatistics();
+                }
+                else if (choice == "x")
+                {
+                    this.quit = true;
+                }
             }
         }
-
-        //number guess game
     }
 
     //Cheat controller
     class CheatController
     {
-        private bool inf_lives = false;
         private bool not_random = false;
+        private bool inf_lives = false;
+        private bool added_cheat = false;
+        public void AddCheat(string password)
+        {
+            if (password == "ch0053#" && !not_random)
+            {
+                this.not_random = true;
+                this.added_cheat = true;
+            }
+            if (password == "und131ng" && !inf_lives)
+            {
+                this.inf_lives = true;
+                this.added_cheat = true;
+            }
+            if (!this.added_cheat)
+            {
+                Console.WriteLine("Invalid Cheat Code or that cheat is already enabled!");
+            }
+        }
+        public bool[] RetriveCheats()
+        {
+            bool[] cheats = new bool[] { this.not_random, this.inf_lives };
+            return cheats;
+        }
+
     }
     //number guess game
 }
--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -7,45 +7,31 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            //run game controller           
-        }
-    }
-
-    // keep track of player data games played cheating lives in each game
-    class PlayerStatistics
-    {
-        private string player_first;
-        private string player_last;
-        private int[] guessing_game = new int[] { 0, 0, 0, 0 };
-        private int lives = 4;
-
-        public PlayerStatistics(string first, string last)
-        {
-
-            player_first = first;
-            player_last = last;
-        }
-
-        //statistics generator
-        public void GenerateStatistics()
-        {
-            List<string> game_list = new List<string>() { "*                Guessing Game                *" };
-            //List<int[]> stats_list = new List<int[]>(){};
-            // for times sake will have to adapt this to add games to a list and loop through
-            //if i have time i will make it generate spacing based off word length
-            Console.Clear();
-            string top_bot = "***********************************************";
-            Console.WriteLine("* Name: {1}, {0} *", player_first, player_last);
-            for (int i = 0; i == game_list.Count; i++)
+            //run profile creation
+            string first = "";
+            string last = "";
+            while (first == "" || last == "")
             {
-                Console.WriteLine(top_bot);
-                Console.WriteLine("*                                             *");//45
-                Console.WriteLine(game_list[i]);
-                Console.WriteLine("*Played: {0}                                *", "00" + guessing_game[i]);//make sure all numbers come out to XXX ie 001, 002, 201, 999 max
-                Console.WriteLine("*Won: {0}                        Lost: {1}*", "00" + guessing_game[i + 1], "00" + guessing_game[i + 2]);
-                Console.WriteLine("*Cheated: {0}                               *", "00" + guessing_game[i + 3]);
+                if (first == "")
+                {
+                    Console.Clear();
+                    Console.Write("Enter first name: ");
+                    first = Console.ReadLine();
+                }
+                else if (last == "")
+                {
+                    Console.Clear();
+                    Console.Write("Enter last name: ");
+                    last = Console.ReadLine();
+                }
+
             }
-            Console.WriteLine(top_bot);
+            PlayerStatistics new_player = new PlayerStatistics(first, last);
+            ProgramController start_playing = new ProgramController(new_player);
+
+            start_playing.Start();
+            Console.WriteLine("Thanks for playing. Press enter to exit.");
+            Console.ReadLine();
         }
     }
 }
