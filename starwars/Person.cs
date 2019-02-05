7994c04ba7d9c1c212098f3e1adaad436795ba6d
unknown <brettj85@gmail.com>
2019-02-04 20:23:32 -0600

    person and ship structure done

---
+++ starwars/Person.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace starwars
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public string alliance;
+
+        public Person(string firstName, string lastName, string alliance)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.alliance = alliance;
+        }
+        public string FullName
+        {
+            get
+            {
+                return this.firstName + " " + this.lastName;
+            }
+
+            set
+            {
+                string[] names = value.Split(' ');
+                this.firstName = names[0];
+                this.lastName = names[1];
+            }
+        }
+    }
+
+}
---
+++ starwars/Ship.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace starwars
+{
+    class Ship
+    {
+        private Person[] passengers;
+        public Ship(string alliance, string type, int size)
+        {
+            this.Type = type;
+            this.Alliance = alliance;
+            this.passengers = new Person[size];
+        }
+
+        public string Type { get; set; }
+
+        public string Alliance { get; set; }
+
+        public string Passengers
+        {
+            get
+            {
+                foreach (var person in passengers)
+                {
+                    Console.WriteLine(String.Format("{0}", person.FullName));
+                }
+
+                return "That's Everybody!";
+            }
+        }
+
+        public void EnterShip(Person person, int seat)
+        {
+            this.passengers[seat] = person;
+        }
+
+        public void ExitShip(int seat)
+        {
+            this.passengers[seat] = null;
+        }
+    }
+}
+}
