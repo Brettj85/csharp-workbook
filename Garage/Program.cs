e526ca91265c55602ff6088fe8947da06609c532
unknown <brettj85@gmail.com>
2019-02-10 20:31:30 -0600

    Garage Done

--- Garage/Program.cs
+++ Garage/Program.cs
@@ -6,7 +6,21 @@ namespace Garage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Person jan = new Person("Jan", "Spitzer");
+            Person george = new Person("George", "Lopez");
+            Person frank = new Person("Frank", "Smith");
+            Car ferarri = new Car("red", 4);
+            ferarri.addPerson(jan, 1);
+            ferarri.addPerson(george, 0);
+            ferarri.addPerson(frank, 3);
+            Car blueCar = new Car("blue", 4);
+            ParkingGarage smallGarage = new ParkingGarage(2);
+
+            smallGarage.ParkCar(blueCar, 0);
+            smallGarage.ParkCar(ferarri, 1);
+            Console.WriteLine(smallGarage.Cars);
+            Console.ReadLine();
         }
     }
 }
