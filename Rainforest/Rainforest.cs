5cf07655b5193eff80afa0e197b3eac38dc6fcb3
unknown <brettj85@gmail.com>
2019-02-11 19:36:45 -0600

    not working

--- Rainforest/Rainforest.cs
+++ Rainforest/Rainforest.cs
@@ -4,9 +4,29 @@ namespace Rainforest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
+            Company ourCompany = new Company();
+            ourCompany.HarvestFruit("strawberrys");
+            ourCompany.HarvestFruit("oranges");
+            ourCompany.HarvestFruit("pinapples");
+            ourCompany.HarvestFruit("apples");
+            ourCompany.HarvestFruit("kiwis");
+            ourCompany.HarvestFruit("banannas");
+            ourCompany.HarvestFruit("pears");
+            ourCompany.HarvestFruit("coconuts");
+            ourCompany.HarvestFruit("starfruits");
+            ourCompany.BuildWarehouse("Austin");
+            ourCompany.BuildWarehouse("New York City");
+            ourCompany.BuildWarehouse("Los Angeles");
+            ourCompany.BuildContainer("Austin");
+            ourCompany.BuildContainer("Austin");
+            ourCompany.BuildContainer("Austin");
+            ourCompany.BuildContainer("New York City");
+            ourCompany.BuildContainer("Los Angeles");
+            ourCompany.BuildContainer("Los Angeles");
+            ourCompany.ProcessFruit("austin-1")
         }
     }
 }
