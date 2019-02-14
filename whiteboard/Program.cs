3ca50e37a30e7f67b14009062956bcef73b334a2
unknown <brettj85@gmail.com>
2019-02-13 22:51:57 -0600

    done with whiteboard

--- whiteboard/Program.cs
+++ whiteboard/Program.cs
@@ -8,12 +8,24 @@ namespace whiteboard
         {
             StringTransform Reverse = new StringTransform();
             Uppercase makeUpper = new Uppercase();
+            LowerCase makeLower = new LowerCase();
             Console.WriteLine("Enter a word.");
             string request = Console.ReadLine();
             string newWord = Reverse.reverseWord(request);
             Console.WriteLine("your word backwards is: {0}", newWord);
             newWord = makeUpper.reverseWord(request);
             Console.WriteLine("your word backwards is: {0}", newWord);
+
+            StringTransform[] arrTransform = new StringTransform[]
+            {
+                makeUpper,
+                makeLower
+            };
+            foreach (var item in arrTransform)
+            {
+                string value = item.reverseWord("ThisString");
+                Console.WriteLine(value);
+            }
         }
     }
 }
