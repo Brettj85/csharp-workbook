88f848b6e53b9478269bac7934dc4b4cbf0cdd94
unknown <brettj85@gmail.com>
2019-02-14 21:13:55 -0600

    starting a new directory

---
+++ Company/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompanyRainforrest Rainforrest = new CompanyRainforrest();
+            Rainforrest.InventoryManagement();
+        }
+    }
+}
---
+++ Company/Rainforrest.cs
@@ -0,0 +1,7 @@
+namespace company
+{
+    class Rainforrest
+    {
+
+    }
+}
