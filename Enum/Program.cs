681de744ba7ebe437aa4e72c751c426ab25b62cf
unknown <brettj85@gmail.com>
2019-02-25 21:11:22 -0600

    ienums

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
-namespace enum
+namespace ENUM
 {
     class Program
     {
+        private enum Day
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+        private enum Months : byte
+        {
+            Jan,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul,
+            Aug,
+            Sep,
+            Oct,
+            Nov,
+            Dec
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Day today = Day.Monday;
+            int dayNumber = (int)today;
+            Console.WriteLine("{0} is day #{1}.", today, dayNumber);
+            Months thisMonth = Months.Dec;
+            byte monthNumber = (byte)thisMonth;
+            Console.WriteLine("{0} is month #{1}.", thisMonth, monthNumber);
+
+            foreach (var item in new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 })
+            {
+                if (Enum.IsDefined(typeof(Months), item))
+                {
+                    var monthEnum = (Months)item;
+                    Console.WriteLine(monthEnum.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Number");
+                }
+            }
         }
     }
 }
