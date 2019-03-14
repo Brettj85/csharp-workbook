4875594a7763a5bcdbf84df5a8b036f7a6ed03fd
unknown <brettj85@gmail.com>
2019-03-13 19:51:59 -0500

    fleshing out still

--- ToDo/ToDoApp/Display/DisplayController.cs
+++ ToDo/ToDoApp/Display/DisplayController.cs
@@ -1,6 +1,29 @@
-﻿namespace ToDoApp
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApp
 {
     internal class DisplayController
     {
+        private int index = 0
+        private string MenuControllerX(List<string> items)
+        {
+            ConsoleKeyInfo userInput = Console.ReadKey();
+
+            if (userInput.Key == ConsoleKey.DownArrow)
+            {
+                index = index == items.Count - 1 ? 0 : index + 1;
+            }
+            else if (userInput.Key == ConsoleKey.UpArrow)
+            {
+                index = index <= 0 ? items.Count - 1 : index - 1;
+            }
+            else if (userInput.Key == ConsoleKey.Enter)
+            {
+                return items[index];
+            }
+            Console.Clear();
+            return "";
+        }
     }
 }
