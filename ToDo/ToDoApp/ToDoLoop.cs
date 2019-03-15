26551db3462d905c34af4866f4a452b7b3132709
unknown <brettj85@gmail.com>
2019-03-15 13:46:38 -0500

    fleshing out still

--- ToDo/ToDoApp/ToDoLoop.cs
+++ ToDo/ToDoApp/ToDoLoop.cs
@@ -16,12 +16,14 @@ namespace ToDoApp
         public void Run()
         {
             string request = "";
+            int index = 0;
             while (true)
             {
-                while (request != "" && request != "a" && request != "d" && request != "v" && request != "e")
+                while (request != "" || request != "a" || request != "d" || request != "v" || request != "e")
                 {
                     List<string> options = new List<string> { "Add Entry", "Update Entry", "Delete Entry", "View Entry", "Exit" };
-                    DisplayVertical display = new DisplayVertical(options, 0);
+                    DisplayVertical display = new DisplayVertical(options, index);
+                    request = display.SelectTask();
 
 
                     if (request == "e")
@@ -33,16 +35,20 @@ namespace ToDoApp
                     switch (request)
                     {
                         case "a":
-                            task.AddTask(0, DataBase);
+                            index = 0;
+                            task.AddTask(index, DataBase);
                             break;
                         case "u":
-                            task.UpdateTask(0, DataBase);
+                            index = 0;
+                            task.UpdateTask(index, DataBase);
                             break;
                         case "d":
-                            task.RemoveTask(0, DataBase);
+                            index = 0;
+                            task.RemoveTask(index, DataBase);
                             break;
                         case "v":
-                            task.ViewTask(0, DataBase);
+                            index = 0;
+                            task.ViewTask(index, DataBase);
                             break;
                         default:
                             break;
